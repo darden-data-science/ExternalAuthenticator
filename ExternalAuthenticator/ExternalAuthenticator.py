78458296f2ae85370e3777d03fd95e771a3697e7
Michael Albert <albertmichaelj@gmail.com>
2021-09-25 16:39:16 -0400

    Changed to path

--- ExternalAuthenticator/ExternalAuthenticator.py
+++ ExternalAuthenticator/ExternalAuthenticator.py
@@ -22,7 +22,7 @@ class ExternalLoginHandler(BaseHandler):
         else:
             self.log.debug("Cookie present! Checking if user can log in.")
             user = await self.login_user()
-            self.log.debug("Path to clear cookis is %r and domain is %r" % (self.request.uri, self.request.host))
+            self.log.debug("Path to clear cookis is %r and domain is %r" % (self.request.path, self.request.host))
             self.clear_cookie(name=auth_token_name, path=self.request.uri, domain=self.request.host)
             if user is None:
                 raise web.HTTPError(403, log_message="Invalid login attempt.")
